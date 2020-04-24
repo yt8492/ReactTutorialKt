9481262adedb61d1027166f93ce4dcb020d300a5
yt8492 <tmymyut2000@gmail.com>
2020-04-24 20:17:54 +0900

    チュートリアル終了

--- src/main/kotlin/component/Board.kt
+++ src/main/kotlin/component/Board.kt
@@ -8,30 +8,12 @@ fun RBuilder.board(
     state: GameState,
     onClick: (i: Int) -> Unit
 ) {
-    val status = when (state) {
-        is GameState.Playing -> {
-            "Next Player: ${state.turn}"
-        }
-        is GameState.End -> {
-            val winner = state.winner
-            if (winner == null) {
-                "Draw"
-            } else {
-                "Winner: $winner"
-            }
-        }
-    }
-    div {
-        div(classes = "status") {
-            + status
-        }
-        (0..2).forEach { i ->
-            div(classes = "board-row") {
-                (0..2).forEach { j ->
-                    val index = i * 3 + j
-                    square(state.board[index]) {
-                        onClick(index)
-                    }
+    (0..2).forEach { i ->
+        div(classes = "board-row") {
+            (0..2).forEach { j ->
+                val index = i * 3 + j
+                square(state.board[index]) {
+                    onClick(index)
                 }
             }
         }
--- src/main/kotlin/component/Game.kt
+++ src/main/kotlin/component/Game.kt
@@ -1,10 +1,10 @@
 package component
 
+import kotlinx.html.js.onClickFunction
 import model.GameState
 import model.Player
 import react.*
-import react.dom.div
-import react.dom.ol
+import react.dom.*
 
 class Game : RComponent<RProps, Game.State>() {
     init {
@@ -12,33 +12,67 @@ class Game : RComponent<RProps, Game.State>() {
             val board = List(9) {
                 null
             }
-            gameState = GameState.Playing(Player.X, board)
+            gameStateHistory = listOf(GameState.Playing(Player.X, board))
+            currentStep = gameStateHistory.lastIndex
         }
     }
 
     override fun RBuilder.render() {
+        val history = state.gameStateHistory.slice(0..state.currentStep)
+        val gameState = history.last()
+        val statusText = when (gameState) {
+            is GameState.Playing -> {
+                "Next Player: ${gameState.turn}"
+            }
+            is GameState.End -> {
+                val winner = gameState.winner
+                if (winner == null) {
+                    "Draw"
+                } else {
+                    "Winner: $winner"
+                }
+            }
+        }
         div(classes = "game") {
             div(classes = "game-board") {
-                board(state.gameState) { i ->
-                    if (state.gameState.board[i] != null) {
+                board(gameState) { i ->
+                    if (gameState.board[i] != null) {
                         return@board
                     }
-                    val playing = state.gameState as? GameState.Playing
+                    val playing = gameState as? GameState.Playing
                     if (playing != null) {
                         setState {
-                            gameState = playing.calculateNextState(i)
+                            gameStateHistory = history + playing.calculateNextState(i)
+                            currentStep = gameStateHistory.lastIndex
                         }
                     }
                 }
             }
             div(classes = "game-info") {
-                div {}
-                ol {}
+                div {
+                    + statusText
+                }
+                ol {
+                    state.gameStateHistory.forEachIndexed { i, _ ->
+                        li {
+                            key = "$i"
+                            button {
+                                + "Go to move #$i"
+                                attrs.onClickFunction = {
+                                    setState {
+                                        currentStep = i
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
 
     interface State : RState {
-        var gameState: GameState
+        var gameStateHistory: List<GameState>
+        var currentStep: Int
     }
 }
