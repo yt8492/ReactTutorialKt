33dfd51c6d6997361a88a5136dbd84a707a1357b
yt8492 <tmymyut2000@gmail.com>
2020-05-04 23:38:35 +0900

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,8 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-js"))
-    implementation("org.jetbrains:kotlin-react:16.13.0-pre.97-kotlin-1.3.72")
-    implementation("org.jetbrains:kotlin-react-dom:16.13.0-pre.97-kotlin-1.3.72")
+    implementation("org.jetbrains:kotlin-react:16.13.1-pre.104-kotlin-1.3.72")
+    implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.104-kotlin-1.3.72")
     implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.7.1")
     implementation(npm("react", "16.13.0"))
     implementation(npm("react-dom", "16.13.0"))
